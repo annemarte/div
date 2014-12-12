5633f858a1b89a973f333d7859b4b330bb05bd24
Anne Marte Hjemaas <anne-marte.hjemas@telenor.com>
2014-12-12 13:44:58 +0100

    before 3.3.2

--- spa/js/spa.js
+++ spa/js/spa.js
@@ -14,7 +14,7 @@
 
 var spa = (function () {
     var initModule = function ( $container ) {
-        spa.shell.initModule($container)
+        spa.shell.initModule($container);
     };
     return { initModule: initModule };
 }());
